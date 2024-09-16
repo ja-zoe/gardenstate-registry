6d0a547199601a792f2f89748e3fa8d8eedcf2fa
ja-zoe <j.vilfort01@gmail.com>
2024-09-15 20:32:27 -0400

    Changes made

--- src/app/api/get-municipality-data/route.ts
+++ src/app/api/get-municipality-data/route.ts
@@ -1,19 +1,22 @@
 import puppeteer from 'puppeteer'
+import pdf from 'pdf-parse'
 
 interface PropertyDataType {
-    propertyName: null | string,
-    propertyOwner: null | string,
-    ownerContact: null | string,
-    
+    href: string,
+    ownerContactName: string,
+    propertyOwnerName: string,
+    address: string
 }
 
 export async function POST(req: Request) {
+    console.log('yuhs')
     const municipality = await req.json() as string
 
     const browser = await puppeteer.launch({
-        headless: true
+        headless: false
     })
     const page = await browser.newPage()
+
     await page.goto('https://njdcaportal.dynamics365portals.us/ultra-bhi-home/ultra-bhi-propertysearch/')
 
     const value = await page.evaluate((municipality) => {
@@ -28,17 +31,62 @@ export async function POST(req: Request) {
 
     await page.waitForSelector('#piListBody a')
 
-    const registrationLinks = await page.evaluate(() => {
-        const linkTags = Array.from(document.querySelectorAll('#piListBody a'))
-        const linkTagHrefs = linkTags.map(link => link.getAttribute('href'))
-        return linkTagHrefs
+    let houseData: PropertyDataType[] = await page.evaluate(() => {
+        const trItems = Array.from(document.querySelectorAll('#piListBody tr'))
+        const linksAndAddresses = trItems.map(tr => {
+            const aTag = tr.querySelector('a')
+            const href = aTag?.getAttribute('href') ?? 'No href data'
+            const location = tr.querySelector('td:nth-child(3)')
+            const locationData = location?.innerHTML ?? 'No address information'
+            return {
+                href,
+                address: locationData,
+                ownerContactName: '',
+                propertyOwnerName: ''
+            }
+        })
+        return linksAndAddresses
     })
 
-    let houseData = []
-    for (const link of registrationLinks) {
-        await page.goto(`https://njdcaportal.dynamics365portals.us${link}`)
-        let houseInfo: = {}
+    let houseIndex = 0
+    for (const house of houseData) {
+        await page.goto(`https://njdcaportal.dynamics365portals.us${house.href}`)
+        const pageInfo = await page.evaluate(() => {
+            const propertyOwnerElement = document.querySelector('#ultra_propertyowner_name')
+            const propertyOwnerName = propertyOwnerElement?.getAttribute('value') ?? 'No property owner information'
+            const ownerContactElement = document.querySelector('#ultra_ownercontact_name')
+            let ownerContactName = ownerContactElement?.getAttribute('value') ?? 'No owner contact information'
+            const registrationButton = document.querySelector('#certificateOfRegistrationBtn') 
+            const href = registrationButton?.getAttribute('id')
+            const isDisabled = registrationButton?.getAttribute('disabled')
+            return {
+                ownerContactName,
+                propertyOwnerName,
+                registrationButton: {
+                    href,
+                    isDisabled
+                }
+            }
+        })
+        houseData[houseIndex].ownerContactName = pageInfo.ownerContactName
+        houseData[houseIndex].propertyOwnerName = pageInfo.propertyOwnerName
+
+        console.log(pageInfo.registrationButton)
+        if (pageInfo.registrationButton.isDisabled) continue
+        page.locator('#certificateOfRegistrationBtn').click()
+
+        console.log(pageInfo)
+        // const response = await page.goto(pageInfo.registrationButton.href as string)
+        // const pdfBuffer = await response?.buffer();
+
+        // if (pdfBuffer) {
+        //     const data = await pdf(pdfBuffer);
+        //     console.log('PDF Text:', data.text);
+        // }
+
+        houseIndex++
     }
+    console.log(houseData)
     await browser.close
     return new Response('yellow')
 }
